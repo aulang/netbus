f4564103bf9294bd74f49e8affacd587fd37f579
Aulang <aulang@qq.com>
2020-11-15 11:22:46 +0800

    BUG修复

--- config/config.go
+++ config/config.go
@@ -4,6 +4,7 @@ import (
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
+	"os"
 	"path/filepath"
 )
 
@@ -25,12 +26,16 @@ type Yaml struct {
 var Config = new(Yaml)
 
 func init() {
-	// 获取文件相对于当前工作目录的相对路径
-	configFilePath, err := filepath.Abs("config.yml")
+	// 获取可执行文件相对于当前工作目录的相对路径
+	root := filepath.Dir(os.Args[0])
 
+	// 根据相对路径获取可执行文件的绝对路径
+	root, err := filepath.Abs(root)
 	if err != nil {
 		log.Fatalf("加载配置文件失败，%v\n", err)
 	}
+
+	configFilePath := root + string(filepath.Separator) + "config.yml"
 
 	configFile, err := ioutil.ReadFile(configFilePath)
 
